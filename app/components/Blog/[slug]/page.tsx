321e52158e93ba8cb625ea6f44107f275168e224
Emmanuel Alanis <ealanisln@me.com>
2023-11-19 12:16:10 -0800

    checkpoint commit

--- app/components/Blog/[slug]/page.tsx
+++ app/components/Blog/[slug]/page.tsx
@@ -1,10 +1,13 @@
 // ./nextjs-app/app/[slug]/page.tsx
 
 import { SanityDocument } from "@sanity/client";
+import { draftMode } from "next/headers";
 import Post from "../Post";
 import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { sanityFetch, token } from "@/sanity/lib/sanityFetch";
 import { client } from "@/sanity/lib/client";
+import PreviewProvider from "../PreviewProvider";
+import PreviewPost from "../PreviewPost";
 
 // Prepare Next.js to know which routes already exist
 export async function generateStaticParams() {
@@ -16,6 +19,15 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: { params: any }) {
   const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
+  const isDraftMode = draftMode().isEnabled;
+
+  if (isDraftMode && token) {
+    return (
+      <PreviewProvider token={token}>
+        <PreviewPost post={post} />
+      </PreviewProvider>
+    );
+  }
 
   return <Post post={post} />;
 }

b29fa40c8e29f8a8293620d1da128c07994a9650
Emmanuel Alanis <ealanisln@me.com>
2023-11-19 12:20:26 -0800

    installed sanity studio

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,22 +1,10 @@
-import { createClient } from '@sanity/client/stega'
+import { createClient } from 'next-sanity'
 
-import {
-  apiVersion,
-  dataset,
-  projectId,
-  revalidateSecret,
-  studioUrl,
-} from '@/sanity/lib/api'
+import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
   apiVersion,
-  // If webhook revalidation is setup we want the freshest content, if not then it's best to use the speedy CDN
-  useCdn: revalidateSecret ? false : true,
-  perspective: 'published',
-  stega: {
-    studioUrl,
-    // logger: console,
-  },
+  dataset,
+  projectId,
+  useCdn,
 })

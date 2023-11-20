0605d903cfb489c10d67e4f3879f5a1c9aee481e
Emmanuel Alanis <ealanisln@me.com>
2023-11-19 16:14:04 -0800

    added blog post to the index

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,38 @@
-import Banner from './components/Banner/index';
+import Banner from "./components/Banner/index";
 // import Construction from './components/Construction/index';
-import People from './components/People/index';
-import Features from './components/Features/index';
-import Blog from './components/Blog/index';
-import Business from './components/Business/index';
-import Payment from './components/Payment/index';
-import Location from './components/Location/index';
+import People from "./components/People/index";
+import Features from "./components/Features/index";
+import Posts from "./components/Blog/Posts";
+import Business from "./components/Business/index";
+import Payment from "./components/Payment/index";
+import Location from "./components/Location/index";
+import { SanityDocument } from "next-sanity";
+import { sanityFetch, token } from "@/sanity/lib/sanityFetch";
+import { postPathsQuery, postsQuery } from "@/sanity/lib/queries";
 
+import { client } from "@/sanity/lib/client";
 
-export default function Home() {
+// Prepare Next.js to know which routes already exist
+export async function generateStaticParams() {
+  // Important, use the plain Sanity Client here
+  const posts = await client.fetch(postPathsQuery);
+
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await sanityFetch<SanityDocument[]>({ query: postsQuery });
+
   return (
     <main>
       {/* <Construction /> */}
       <Banner />
       <People />
       <Features />
-      <Blog />
+      <Posts posts={posts} />
       <Business />
       <Payment />
       <Location />
     </main>
-  )
+  );
 }

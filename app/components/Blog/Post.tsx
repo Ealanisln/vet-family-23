9053eca3a957e0adc653c86de5bad67dec07e0b7
Emmanuel Alanis <ealanisln@me.com>
2023-11-19 22:23:49 -0800

    added social share for blog posts

--- app/components/Blog/Post.tsx
+++ app/components/Blog/Post.tsx
@@ -5,6 +5,19 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityDocument } from "@sanity/client";
 import { PortableText } from "@portabletext/react";
 import { client } from "@/sanity/lib/client";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  PinterestShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TumblrShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  XIcon,
+} from "react-share";
 
 const builder = imageUrlBuilder(client);
 
@@ -16,8 +29,14 @@ export default function Post({ post }: { post: SanityDocument }) {
     return <div>Loading...</div>; // or any other fallback UI
   }
 
+  const shareUrl =
+    typeof window !== "undefined"
+      ? window.location.href
+      : "http://www.familyvet23.com";
+  const title = post.title;
+
   return (
-    <main className="container mx-auto prose prose-lg p-4">
+    <main className="container mx-auto prose prose-lg py-24">
       <h1>{post.title || "Untitled"}</h1>
       {post?.mainImage ? (
         <Image
@@ -29,6 +48,29 @@ export default function Post({ post }: { post: SanityDocument }) {
         />
       ) : null}
       {post?.body ? <PortableText value={post.body} /> : null}
+      <hr className="my-8 border-t border-gray-400" />
+
+      <h3 className="text-2xl sm:text-xl font-semibold text-black text-left my-10">
+        Comparte este artículo en redes sociales:
+      </h3>
+
+
+      <div className="flex space-x-4">
+        <TwitterShareButton
+          url={shareUrl}
+          title={title}
+          className="Demo__some-network__share-button"
+        >
+          <XIcon size={32} round />
+        </TwitterShareButton>
+
+        <FacebookShareButton
+          url={shareUrl}
+          className="Demo__some-network__share-button"
+        >
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+      </div>
     </main>
   );
 }

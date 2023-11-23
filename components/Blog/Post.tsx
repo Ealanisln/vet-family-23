96aa6cd19e2e9b4fe82cde94fe7b4d34ccd2670b
Emmanuel Alanis <ealanisln@me.com>
2023-11-23 11:18:22 -0800

    fixed share buttons on blog

--- components/Blog/Post.tsx
+++ components/Blog/Post.tsx
@@ -5,19 +5,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityDocument } from "@sanity/client";
 import { PortableText } from "@portabletext/react";
 import { client } from "@/sanity/lib/client";
-import {
-  EmailShareButton,
-  FacebookShareButton,
-  FacebookIcon,
-  LinkedinShareButton,
-  PinterestShareButton,
-  RedditShareButton,
-  TelegramShareButton,
-  TumblrShareButton,
-  TwitterShareButton,
-  WhatsappShareButton,
-  XIcon,
-} from "react-share";
+import Share from "./Share";
 
 const builder = imageUrlBuilder(client);
 
@@ -28,12 +16,6 @@ export default function Post({ post }: { post: SanityDocument }) {
     // Handle the case when post is null or undefined
     return <div>Loading...</div>; // or any other fallback UI
   }
-
-  const shareUrl =
-    typeof window !== "undefined"
-      ? window.location.href
-      : "http://www.familyvet23.com";
-  const title = post.title;
 
   return (
     <main className="container mx-auto prose prose-lg py-24">
@@ -49,28 +31,7 @@ export default function Post({ post }: { post: SanityDocument }) {
       ) : null}
       {post?.body ? <PortableText value={post.body} /> : null}
       <hr className="my-8 border-t border-gray-400" />
-
-      <h3 className="text-2xl sm:text-xl font-semibold text-black text-left my-10">
-        Comparte este artículo en redes sociales:
-      </h3>
-
-
-      <div className="flex space-x-4">
-        <TwitterShareButton
-          url={shareUrl}
-          title={title}
-          className="Demo__some-network__share-button"
-        >
-          <XIcon size={32} round />
-        </TwitterShareButton>
-
-        <FacebookShareButton
-          url={shareUrl}
-          className="Demo__some-network__share-button"
-        >
-          <FacebookIcon size={32} round />
-        </FacebookShareButton>
-      </div>
+      <Share post={post} />
     </main>
   );
 }

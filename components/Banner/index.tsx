4d7faffef71a86a142d7fe822c415fce54f9b76e
Emmanuel Alanis <ealanisln@me.com>
2024-01-12 14:49:43 -0800

    :fire: Banner updated

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -34,7 +34,7 @@ const Banner = () => {
 
           <div className="col-span-5 flex justify-center xl:-mb-24 xl:-mr-32 pt-10 lg:pt-0">
             <Image
-              src="/assets/banner/banner-navidad.png"
+              src="/assets/banner/banner.png"
               alt="hero"
               width={900}
               height={805}

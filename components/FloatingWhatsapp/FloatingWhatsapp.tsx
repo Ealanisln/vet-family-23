231a9650c9bed3b13416c47817bf7525df4c6b94
Emmanuel Alanis <ealanisln@me.com>
2024-04-11 13:55:50 -0700

    :sparkles: Added whatsapp button

---
+++ components/FloatingWhatsapp/FloatingWhatsapp.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import  FloatingSsr  from "./FloatingSsr";
+
+export default function FloatingWhatsapp() {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+  return (
+    <div>
+      {isSSR ? null : (
+        <FloatingSsr
+          phoneNumber="4772605743"
+          accountName="Vet for Family"
+          allowEsc
+          notification
+        />
+      )}
+    </div>
+  );
+}

363018a3edb971d57d128358c24f89c008e3a14d
Emmanuel Alanis <ealanisln@me.com>
2024-04-11 13:55:50 -0700

    :sparkles: Added whatsapp button

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -8,6 +8,10 @@ import Location from "../../components/Location/";
 import Contact, { FormProps } from "../../components/Contact";
 import GaleriaFotos from "@/components/GaleriaFotos/GaleriaFotos";
 // import Specials from "@/components/Specials";
+import FloatingWhatsApp from "@/components/FloatingWhatsapp/FloatingWhatsapp";
+import { Suspense } from "react";
+import { LoadingSpinner } from "@/components/ui/";
+import FloatingSsr from "@/components/FloatingWhatsapp/FloatingSsr";
 
 export default async function Home() {
   const formProps: FormProps = {
@@ -19,6 +23,7 @@ export default async function Home() {
 
   return (
     <main>
+      <FloatingWhatsApp />
       <Banner />
       {/* <Specials /> */}
       <Reviews />
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,3 +3,4 @@ export * from './card';
 export * from './carousel';
 export * from './dialog';
 export * from './drawer';
+export * from './spinner';

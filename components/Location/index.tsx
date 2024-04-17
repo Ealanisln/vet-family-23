04a85c62a1146034344331ba7a211d72f0c87dbf
Emmanuel Alanis <ealanisln@me.com>
2024-04-17 09:45:10 -0700

    :bug: Fixed some bugs
    Maps, banner image

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Links from "./Links";
-import Map from "./Map";
+import { Map } from "./Map";
 
 const index = () => {
   return (
     <>
       <Links />
-      <Map />
+      <div className="mx-auto max-w-2xl pt-8 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <Map />
+      </div>
     </>
   );
 };

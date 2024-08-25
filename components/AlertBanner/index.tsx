c2f62b425f0bd5b92a7f62900973163880b1fdf4
Emmanuel Alanis <ealanisln@me.com>
2024-08-25 15:54:49 -0700

    :lipstick: Update banner

--- components/AlertBanner/index.tsx
+++ components/AlertBanner/index.tsx
@@ -5,15 +5,27 @@ import { AlertTriangle } from "lucide-react";
 
 export function AlertBanner() {
   return (
-    <div className="bg-yellow-100 border-l-4 border-yellow-500 p-4">
+    <div
+      className="bg-yellow-100 border-l-4 border-yellow-500 p-4 mt-8 md:mt-0
+    "
+    >
       <div className="flex items-center">
         <AlertTriangle className="h-6 w-6 text-yellow-500 mr-4" />
         <div className="flex-1">
-          <p className="font-bold">
-            Alerta: Nuevos requisitos para mascotas que ingresan a los Estados
-            Unidos.
-          </p>
-          <p className="text-sm">
+          <div className="flex flex-col sm:flex-row items-start sm:items-center">
+            <Image
+              src="/assets/banner/us-flag.svg"
+              alt="US Flag"
+              width={30}
+              height={20}
+              className="mb-2 sm:mb-0"
+            />
+            <p className="font-bold text-left sm:text-left sm:ml-2">
+              Chips para mascotas: Nuevos requisitos para mascotas que ingresan a los Estados
+              Unidos.
+            </p>
+          </div>
+          <p className="text-sm text-left sm:text-left">
             Importante información para viajeros con mascotas
           </p>
         </div>
